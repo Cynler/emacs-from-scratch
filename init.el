dd8cb7ca4d370ba62f4f71d5321065e51ba21573
clu <lucythi@gmail.com>
2022-09-15 17:12:00 -0400

    Emacs From Scratch 4, updated to Emacs28 settings

--- init.el
+++ init.el
@@ -22,8 +22,7 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")))
 
 (package-initialize)
 (unless package-archive-contents
